ffa57d6091a38b7e2cff60eeff93e80c4245adf1
Pierre Luc Carrier <carriepl@grincheux.iro.umontreal.ca>
2012-06-28 16:01:04 -0400

    Added some tests for WeightActs

--- tests/test_unshared_conv_diagonally.py
+++ tests/test_unshared_conv_diagonally.py
@@ -182,18 +182,24 @@ class TestWeightActs(unittest.TestCase):
 
     #Each item in ishape_list : (icount, icolors, irows, icols)
     #ishape_list = [(1, 1, 98, 98),(2, 3, 24, 24)]
-    ishape_list = [(10, 1, 98, 98)]
+    ishape_list = [(1, 2, 6, 6), (1, 2, 6, 6),
+                   (2, 3, 24, 24), (2, 3, 20, 20),
+                   (2, 3, 49, 49), (20, 1, 98, 98)]
 
     #Each item in fshapes_list = (fmodules, filters_per_module,
     #                             fcolors, frows, fcols)
 
     #fshape_list = [(11, 32, 1, 11, 11),(1, 1, 3, 6, 6)]
-    fshape_list = [(11, 32, 1, 11, 11)]
+    fshape_list = [(1, 1, 2, 3, 3), (1, 1, 2, 3, 3),
+                   (1, 1, 3, 6, 6), (3, 2, 3, 6, 6),
+                   (6, 32, 3, 11, 11), (11, 32, 1, 11, 11)]
 
     # Each item in hshapes_list = (hcount, fmodules, filter_per_module,
     #                              hrows, hcols)
     #hshape_list = [(1, 11, 32, 8, 8),(2, 1, 1, 6, 6 )]
-    hshape_list = [(10, 11, 32, 8, 8)]
+    hshape_list = [(1, 1, 1, 2, 2), (1, 1, 1, 2, 2),
+                   (2, 1, 1, 4, 4), (2, 3, 2, 3, 3),
+                   (2, 6, 32, 4, 4), (20, 11, 32, 8, 8)]
     
 
 
